86a61504db575ef78ea512564445249fb362b6e2
Grey Pegram <dpegram85@gmail.com>
2024-10-30 22:54:07 -0400

    Completed Optional Lab

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,7 @@
-def parrot():
-    pass
+def parrot(string=None, default_string="Squawk!"):
+    if not string:
+        print(default_string)
+        return(default_string)
+    else:    
+        print(string)
+        return string
